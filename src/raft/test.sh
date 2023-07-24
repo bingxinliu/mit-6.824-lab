c3711f2e45b8ff960f6e06f620f644bbb7b9130c
liubingxin30 <bingxinliu30@gmail.com>
2023-07-24 15:24:56 +0800

    modified/added files for test

--- src/raft/test.sh
+++ src/raft/test.sh
@@ -1,19 +1,40 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 numTrials"
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 numTrials testname"
     exit 1
 fi
 
-trap 'kill -INT -$pid; exit 1' INT
+trap 'exit 1' INT
+
+# runs=$1
+# for i in $(seq 1 $runs); do
+#     timeout -k 2s 900s go test -run 2A | tee -a 2A.tmplog &
+#     pid=$!
+#     if ! wait $pid; then
+#         echo '***' FAILED TESTS IN TRIAL $i
+#         exit 1
+#     fi
+# done
+# echo '***' PASSED ALL $i TESTING TRIALS FOR 2A
 
 runs=$1
 for i in $(seq 1 $runs); do
-    timeout -k 2s 900s go test -run 2A | tee -a output &
-    pid=$!
-    if ! wait $pid; then
-        echo '***' FAILED TESTS IN TRIAL $i
-        exit 1
+    echo "$i / $1"
+    LOG="2B_$i.tmplog"
+    VERBOSE=0 go test -run $2 &> $LOG
+    # if ! wait $pid; then
+    #     echo '***' FAILED TESTS IN TRIAL $i
+    #     # exit 1
+    # fi
+    if [[ $? -ne 0 ]]; then
+        echo "Failed at iter $i, saving log at $LOG"
+        continue
     fi
+    if [[ `cat $LOG | grep FAIL | wc -l` -gt 0 ]]; then
+        echo "FAILED at iter $i, saving log at $LOG"
+        continue
+    fi
+    rm $LOG
 done
-echo '***' PASSED ALL $i TESTING TRIALS
+echo '***' PASSED ALL $i TESTING TRIALS 2B
